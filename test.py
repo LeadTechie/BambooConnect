49ae6c463a377058d6ba5d77d274eeb7dd7ab6c5
coder-chris <coder.chris.github@gmail.com>
2022-06-22 19:09:36 +0200

    integration to test access to google sheets

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import gspread
+import base64
+import json
+import os
 
 file = "data1.csv"
 
@@ -11,3 +15,15 @@ pd.read_csv(file, sep = symbol)
 
 output = df.sort_values(by = "a")
 print(output)
+
+credentialsb64 = os.getenv('CREDENTIALS_JSON')
+credentials = base64.b64decode(credentialsb64).decode('ascii')
+print(credentials)
+print()
+json_credentials = json.loads(credentials)
+print(json_credentials)
+gc = gspread.service_account_from_dict(json_credentials)
+
+sh = gc.open("Recon Tools Test Data")
+
+print(sh.sheet1.get('A1'))
