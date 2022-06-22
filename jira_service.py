b3bf84b8850bb66b30448436a4a3a28c023e2cf0
coder-chris <coder.chris.github@gmail.com>
2022-06-22 22:49:08 +0200

    Added JIRA access to components

--- jira_service.py
+++ jira_service.py
@@ -1,20 +1,76 @@
 # library modules
+import os
 from jira import JIRA
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import datetime
 
-user = 'me@here.com'
-apikey = 'your0api0key0here'
-server = 'https://SITE_NAME.atlassian.net'
+user =  os.getenv('RECON_TOOLS_JIRA_EMAIL')
+apikey = os.getenv('RECON_TOOLS_JIRA_TOKEN')
+#https://leadtechie.atlassian.net/rest/api/3/project/TEST/components
+server = os.getenv('RECON_TOOLS_JIRA_SERVER')
 
-options = {
- 'server': server
-}
 
-jira = JIRA(options, basic_auth=(user,apikey) )
+def testJIRAaccess1():
+    options = {
+     'server': server
+    }
 
-ticket = 'KRP-11697'
-issue = jira.issue(ticket)
+    jira = JIRA(options, basic_auth=(user,apikey) )
 
-summary = issue.fields.summary
+    ticket = 'TEST-3'
+    issue = jira.issue(ticket)
 
-print('ticket: ', ticket, summary)
-Omantha Prasad
+    summary = issue.fields.summary
+
+    print('ticket: ', ticket, summary)
+    return summary
+
+def testJIRAaccess2():
+    url = "https://leadtechie.atlassian.net/rest/api/3/project/TEST/components"
+
+    auth = HTTPBasicAuth(user, apikey)
+
+    headers = {
+        "Accept": "application/json",
+        "X-Atlassian-Token": "no-check"
+    }
+
+    response = requests.request(
+        "GET",
+        url,
+        headers = headers,
+        auth = auth
+        #files = {
+        #     "file": ("myfile.txt", open("myfile.txt","rb"), "application-type")
+        #}
+        )
+    return response
+
+def parseComponentsJSON(components):
+    results = []
+    for component in components:
+        if 'lead' in component:
+            lead = component['lead']['displayName']
+        else:
+            lead = "<No Owner>"
+        # Ruby syntax is nicer! hash['description']?hash['description']:'<No Description>'
+        if 'description' in component:
+            description = component['description']
+        else:
+            description = "<No Description>",
+
+        results.append( [ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            component["id"],
+            component["name"],
+            lead,
+            description
+            ])
+    return results
+
+print()
+print("RESPONSE")
+components = json.loads(testJIRAaccess2().content)
+print(json.dumps(parseComponentsJSON(components), indent=4))
+print("RESPONSE-END")
