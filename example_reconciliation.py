2da7a2901f73b03ba8bc0e33f08407b7ed479317
coder-chris <coder.chris.github@gmail.com>
2022-07-01 09:34:31 +0200

    update, delete, add working

--- example_reconciliation.py
+++ example_reconciliation.py
@@ -2,7 +2,7 @@ from connectors.googlesheets_connector import GoogleSheets_Connector
 from connectors.jira_connector import Jira_Connector
 import pandas as pd
 import json
-
+import numpy as np
 
 def get_external_data():
     gsc = GoogleSheets_Connector()
@@ -40,6 +40,10 @@ def print_dataset(df, name):
     print(df)
     print("")
 
+# https://stackoverflow.com/questions/15891038/change-column-type-in-pandas
+def coerce_df_columns_to_numeric(df, column_list):
+    df[column_list] = df[column_list].apply(pd.to_numeric, errors='coerce')
+
 def get_local_data():
     sheetdf = pd.read_csv('sheetdf.csv', encoding='utf-8')
     jiradf = pd.read_csv('jiradf.csv', encoding='utf-8')
@@ -55,25 +59,35 @@ def get_local_data():
     sheetdf.columns = new_header #Set the header row as the df header
     jiradf.columns = new_header
 
-    sheetdf['id']=sheetdf['id'].astype('object')
-    jiradf['id']=jiradf['id'].astype('object')
+    sheetdf['id']=sheetdf['id'].astype('int')
+    jiradf['id']=jiradf['id'].astype('int')
 
-    jiradf = jiradf.set_index('id', drop=True)
-    sheetdf = sheetdf.set_index('id', drop=True)
+    jiradf = jiradf.set_index('id', drop=False)
+    sheetdf = sheetdf.set_index('id', drop=False)
     #jiradf.set_index('id', drop=False)
     #sheetdf.set_index('id', drop=False)
 
-    jiradf.index = jiradf.index.astype('object')
-    sheetdf.index = sheetdf.index.astype('object')
+    #jiradf = jiradf.rename(columns={"id": "myid"})
+    #sheetdf = sheetdf.rename(columns={"id": "myid"})
+    jiradf.index.name = 'index'
+    sheetdf.index.name = 'index'
+
+    jiradf.index.name = 'index'
+    sheetdf.index.name = 'index'
+
+    #pd.to_numeric(s)
+
+    jiradf.index = jiradf.index.astype('int')
+    sheetdf.index = sheetdf.index.astype('int')
 
     sheetdf = sheetdf.drop(columns=['Time Stamp'])
     jiradf = jiradf.drop(columns=['Time Stamp'])
 
-    print("Meta Data")
-    print(sheetdf.columns)
-    print(jiradf.columns)
-    print(sheetdf.dtypes)
-    print(jiradf.dtypes)
+    #print("Meta Data")
+    #print(sheetdf.columns)
+    #print(jiradf.columns)
+    #print(sheetdf.dtypes)
+    #print(jiradf.dtypes)
 
     print_datasets(sheetdf, jiradf)
     #print(sheetdf.columns)
@@ -84,18 +98,49 @@ def get_local_data():
     #pd.to_numeric(jiradf["id"])
     #pd.to_numeric(sheetdf["id"])
 
-    print("sheetdf.info()")
-    print(sheetdf.info())
+    #print("sheetdf.info()")
+    #print(sheetdf.info())
 
-    print("jiradf.info()")
-    print(jiradf.info())
+    #print("jiradf.info()")
+    #print(jiradf.info())
 
+    # Notes that didn't quite work...
     #https://moonbooks.org/Articles/How-to-replace-rows-of-a-dataframe-using-rows-of-another-dataframe-based-on-indexes-with-pandas-/
-
     #https://stackoverflow.com/questions/51394653/update-a-pandas-dataframe-with-data-from-another-dataframe
-    df = sheetdf.combine_first(jiradf).reindex(jiradf.index)
+    #df = sheetdf.combine_first(jiradf).reindex(jiradf.index)
 
-    print_dataset(df, "result")
+    # Update values where the row is in old and new data sets
+    #sheetdf.update(sheetdf[['id']].merge(jiradf, 'left'))
+
+    in_jira =  jiradf.loc[:,"id"].values
+    in_sheet = sheetdf.loc[:,"id"].values
+    update_rows = np.intersect1d(in_jira, in_sheet)
+    for i in update_rows:
+        sheetdf.loc[i] = jiradf.loc[i]
+
+    # add new lines to the end
+    in_jira =  jiradf.loc[:,"id"].values
+    in_sheet = sheetdf.loc[:,"id"].values
+    new_rows = np.setdiff1d(in_jira, in_sheet)
+    print(in_jira, in_sheet, new_rows)
+    print_dataset(sheetdf, "pre result")
+
+    for i in new_rows:
+        sheetdf.loc[i] = jiradf.loc[i]
+
+    new_rows = np.setdiff1d(in_jira, in_sheet)
+    print(in_jira, in_sheet, new_rows)
+    print_dataset(sheetdf, "pre result")
+
+
+    #dataframe.at[index,'column-name']='new value'
+    deleted_rows = np.setdiff1d(in_sheet, in_jira)
+    print(deleted_rows)
+    for i in deleted_rows:
+        sheetdf.loc[i, 'name':] = ""
+
+    print(sheetdf.dtypes)
+    print_dataset(sheetdf, "result")
 
 #get_external_data()
 get_local_data()
