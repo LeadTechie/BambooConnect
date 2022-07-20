8c22e8e323a57408fd4978d89832e3a5baf0a9ab
coder-chris <coder.chris.github@gmail.com>
2022-07-20 20:58:08 +0200

    test package workflow

---
+++ bamboo_connect/bamboo_connect_test_main.py
@@ -0,0 +1,8 @@
+from bamboo_connect_test_package import test_method
+
+def main():
+    print("test_main")
+    return test_method()
+
+if __name__ == "__main__":
+    main()
---
+++ bamboo_connect/bamboo_connect_test_package.py
@@ -0,0 +1,7 @@
+
+def test_method():
+    print("test")
+    return "test"
+
+if __name__ == "__main__":
+    test_method()
