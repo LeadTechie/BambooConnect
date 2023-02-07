d5988ae6a1f23f4708ea602106c054273bd25584
Chris Rowe <coder.chris.github@gmail.com>
2023-02-07 20:24:05 +0100

    check point. Cleaned up to new file structure

--- bamboo_connect/bamboo_connect_test_main.py
+++ bamboo_connect/bamboo_connect_test_main.py
@@ -1,8 +1,5 @@
-from bamboo_connect_test_package import test_method
-
 def main():
     print("test_main")
-    return test_method()
 
 if __name__ == "__main__":
     main()
--- poc_test.py
+++
@@ -1,8 +0,0 @@
-from connectors.googlesheets_connector import GoogleSheets_Connector
-from transform.recon_dataset import Recon_DataSet
-
-import support.poc_e2e_script as e2e
-
-print("hello")
-e2e.e2e_test()
-print("hello2")
