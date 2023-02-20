c24bf6e25268851da78d5005104b0c965905c778
Chris Rowe <coder.chris.github@gmail.com>
2023-02-20 21:30:13 +0100

    removed test code that tests delay 2s

--- extractors/extractor.py
+++ extractors/extractor.py
@@ -82,7 +82,7 @@ def my_function():
     # Function implementation here
     time.sleep(2)
 
-my_function()
+#my_function()
 # -
 
 
@@ -237,7 +237,7 @@ def test_repeated_calls_to_check_cache():
     pretty_json = json.dumps(components_json, indent=4)
     print(pretty_json)
 
-#test_repeated_calls_to_check_cache()
+test_repeated_calls_to_check_cache()
 # -
 
 
@@ -298,10 +298,6 @@ class Test_Extract_Component_List(unittest.TestCase):
         pretty_json = json.dumps(re_components_json, indent=4)
         self.assertEqual(re_components_json[0], expected, "check first component")
 
-
-#logging.basicConfig(level=logging.ERROR)
-
-#unittest.main(argv=[''], verbosity=2, exit=False)
 
 # +
 
